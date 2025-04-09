d862b4fb66510112746c4c532d7540c9d8f61983
sushi <64286083+sushiroles@users.noreply.github.com>
2025-04-09 21:19:29 +0200

    finished collection search

--- AstroAPI/media_services/music/spotify/__init__.py
+++ AstroAPI/media_services/music/spotify/__init__.py
@@ -2,6 +2,7 @@ from AstroAPI.components import *
 from .components.generic import *
 
 from .components.search.song import search_song
+from .components.search.collection import search_collection
 from .components.lookup.song import lookup_song
 from .components.lookup.collection import lookup_collection
 
@@ -15,6 +16,10 @@ class Spotify:
     async def search_song(self, artists: list, title: str, song_type: str = None, collection: str = None, is_explicit: bool = None, country_code: str = 'us') -> Song | Empty | Error:
         song = await search_song(artists = artists, title = title, song_type = song_type, collection = collection, is_explicit = is_explicit, country_code = country_code)
         return song
+    
+    async def search_collection(self, artists: list, title: str, year: int = None, country_code: str = 'us') -> Collection | Empty | Error:
+        collection = await search_collection(artists = artists, title = title, year = year, country_code = country_code)
+        return collection
 
     async def lookup_song(self, id: str, country_code: str = 'us') -> Song | Empty | Error:
         song = await lookup_song(id = id, country_code = country_code)
