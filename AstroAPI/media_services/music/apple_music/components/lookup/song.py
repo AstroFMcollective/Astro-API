2b580af62e0492c31b69ee67d11336c95204faca
sushi <64286083+sushiroles@users.noreply.github.com>
2025-05-07 23:33:42 +0200

    AM lookup song

--- AstroAPI/media_services/music/apple_music/components/lookup/song.py
+++ AstroAPI/media_services/music/apple_music/components/lookup/song.py
@@ -1,13 +1,16 @@
 from AstroAPI.components import *
 from AstroAPI.media_services.music.apple_music.components.generic import *
+from AstroAPI.media_services.music.apple_music.components.lookup.artist import lookup_artist
+from AstroAPI.media_services.music.apple_music.components.lookup.collection import lookup_collection
+
 import aiohttp
 
 
 
-async def lookup_song(self, id: str, country_code: str = 'us') -> object:
+async def lookup_song(id: str, country_code: str = 'us') -> object:
 	async with aiohttp.ClientSession() as session:
 		request = {'request': 'lookup_song', 'id': id, 'country_code': country_code, 'url': f'https://music.apple.com/{country_code}/album/{id}'}
-		api_url = f'https://itunes.apple.com/lookup'
+		api_url = f'{api}/lookup'
 		api_params = {
 			'id': id,
 			'country': country_code.lower()
@@ -19,42 +22,58 @@ async def lookup_song(self, id: str, country_code: str = 'us') -> object:
 			if response.status == 200:
 				song = await response.json(content_type = 'text/javascript')
 				song = song['results'][0]
-					
 				song_type = 'track' if ' - Single' not in song['collectionName'] else 'single'
 				song_url = song['trackViewUrl']
 				song_id = song['trackId']
 				song_title = song['trackName']
-				song_artists = await self.lookup_artist(id = song['artistId'], country_code = country_code)
-				song_artists = [song_artists.name] if song_artists.type != 'error' else [song['artistName']]
-				song_cover = song['artworkUrl100']
+				song_artists = await lookup_artist(id = song['artistId'], country_code = country_code)
+				song_artists = [song_artists]
 				song_is_explicit = not 'not' in song['trackExplicitness']
-				song_collection = remove_feat(clean_up_collection_title(song['collectionName']))
+				song_genre = song['primaryGenreName'] if 'primaryGenreName' in song else None
+				song_cover = Cover(
+					service = service,
+					media_type = song_type,
+					title = song_title,
+					artists = song_artists,
+					hq_urls = song['artworkUrl100'],
+					lq_urls = song['artworkUrl30'],
+					color_hex = None,
+					meta = Meta(
+						service = service,
+						request = request,
+						processing_time = current_unix_time_ms() - start_time,
+						filter_confidence_percentage = {service: 100.0},
+						http_code = response.status
+					)
+				)
+				song_collection = await lookup_collection(id = song['collectionId'], country_code = country_code)
 				return Song(
-					service = self.service,
+					service = service,
 					type = song_type,
-					url = song_url,
-					id = song_id,
+					urls = song_url,
+					ids = song_id,
 					title = song_title,
 					artists = song_artists,
 					collection = song_collection,
 					is_explicit = song_is_explicit,
-					cover_url = song_cover,
+					cover = song_cover,
+					genre = song_genre,
 					meta = Meta(
-						service = self.service,
+						service = service,
 						request = request,
 						processing_time = current_unix_time_ms() - start_time,
-						filter_confidence_percentage = {self.service: 100.0},
+						filter_confidence_percentage = {service: 100.0},
 						http_code = response.status
 					)
 				)
 
 			else:
 				error = Error(
-					service = self.service,
-					component = self.component,
+					service = service,
+					component = component,
 					error_msg = "HTTP error when looking up song ID",
 					meta = Meta(
-						service = self.service,
+						service = service,
 						request = request,
 						processing_time = current_unix_time_ms() - start_time,
 						http_code = response.status
