8f1cefb4ac1740031d535b99595381717a608aff
sushi <64286083+sushiroles@users.noreply.github.com>
2025-05-07 23:33:35 +0200

    AM lookup collection

---
+++ AstroAPI/media_services/music/apple_music/components/lookup/collection.py
@@ -0,0 +1,144 @@
+from AstroAPI.components import *
+from AstroAPI.media_services.music.apple_music.components.generic import *
+from AstroAPI.media_services.music.apple_music.components.lookup.artist import lookup_artist
+import aiohttp
+
+
+
+
+async def lookup_collection(id: str, country_code: str = 'us') -> object:
+	async with aiohttp.ClientSession() as session:
+		request = {'request': 'lookup_collection', 'id': id, 'country_code': country_code}
+		api_url = f'{api}/lookup'
+		api_params = {
+			'id': id,
+			'country': country_code.lower()
+		}
+		timeout = aiohttp.ClientTimeout(total = 30)
+		start_time = current_unix_time_ms()
+
+		async with session.get(url = api_url, params = api_params, timeout = timeout) as response:
+			if response.status == 200:
+				collection = await response.json(content_type = 'text/javascript')
+				collection = collection['results'][0]
+
+				if ' - Single' not in collection['collectionName']:
+					collection_type = ('album' if ' - EP' not in collection['collectionName'] else 'ep')
+					collection_url = collection['collectionViewUrl']
+					collection_id = collection['collectionId']
+					collection_title = clean_up_collection_title(collection['collectionName'])
+					collection_artists = await lookup_artist(id = collection['artistId'], country_code = country_code)
+					collection_artists = [collection_artists]
+					collection_year = collection['releaseDate'][:4]
+					collection_genre = collection['primaryGenreName'] if 'primaryGenreName' in collection else None
+					collection_cover = Cover(
+						service = service,
+						media_type = collection_type,
+						title = collection_title,
+						artists = collection_artists,
+						hq_urls = collection['artworkUrl100'],
+						lq_urls = collection['artworkUrl60'],
+						color_hex = None,
+						meta = Meta(
+							service = service,
+							request = request,
+							processing_time = current_unix_time_ms() - start_time,
+							filter_confidence_percentage = {service: 100.0},
+							http_code = response.status
+						)
+					)
+					return Collection(
+						service = service,
+						type = collection_type,
+						urls = collection_url,
+						ids = collection_id,
+						title = collection_title,
+						artists = collection_artists,
+						release_year = collection_year,
+						cover = collection_cover,
+						genre = collection_genre,
+						meta = Meta(
+							service = service,
+							request = request,
+							processing_time = current_unix_time_ms() - start_time,
+							filter_confidence_percentage = {service: 100.0},
+							http_code = response.status
+						)
+					)
+				else:
+					song_type = 'single'
+					song_url = collection['collectionViewUrl']
+					song_id = collection['collectionId']
+					song_title = clean_up_collection_title(collection['collectionName'])
+					song_artists = await lookup_artist(id = collection['artistId'], country_code = country_code)
+					song_artists = [song_artists]
+					song_is_explicit = not 'not' in collection['collectionExplicitness']
+					song_genre = collection['primaryGenreName'] if 'primaryGenreName' in collection else None
+					song_cover = Cover(
+						service = service,
+						media_type = song_type,
+						title = song_title,
+						artists = song_artists,
+						hq_urls = collection['artworkUrl100'],
+						lq_urls = collection['artworkUrl30'],
+						color_hex = None,
+						meta = Meta(
+							service = service,
+							request = request,
+							processing_time = current_unix_time_ms() - start_time,
+							filter_confidence_percentage = {service: 100.0},
+							http_code = response.status
+						)
+					)
+					song_collection = Collection(
+						service = service,
+						type = song_type,
+						urls = song_url,
+						ids = song_id,
+						title = collection_title,
+						artists = collection_artists,
+						cover = song_cover,
+						release_year = collection['releaseDate'][:4],
+						genre = song_genre,
+						meta = Meta(
+							service = service,
+							request = request,
+							processing_time = current_unix_time_ms() - start_time,
+							filter_confidence_percentage = {service: 100.0},
+							http_code = response.status
+						)
+					)
+					return Song(
+						service = service,
+						type = song_type,
+						urls = song_url,
+						ids = song_id,
+						title = song_title,
+						artists = song_artists,
+						collection = song_collection,
+						is_explicit = song_is_explicit,
+						cover = song_cover,
+						genre = song_genre,
+						meta = Meta(
+							service = service,
+							request = request,
+							processing_time = current_unix_time_ms() - start_time,
+							filter_confidence_percentage = {service: 100.0},
+							http_code = response.status
+						)
+					)
+
+			else:
+				error = Error(
+					service = service,
+					component = component,
+					error_msg = "HTTP error when looking up collection ID",
+					meta = Meta(
+						service = service,
+						request = request,
+						processing_time = current_unix_time_ms() - start_time,
+						http_code = response.status
+					)
+				)
+				await log(error)
+				return error
