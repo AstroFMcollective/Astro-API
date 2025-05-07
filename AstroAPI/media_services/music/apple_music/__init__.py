9ab76d87c854ff307561e875c167b1e0b128997b
sushi <64286083+sushiroles@users.noreply.github.com>
2025-05-07 23:29:40 +0200

    cleanup

--- AstroAPI/media_services/music/apple_music/__init__.py
+++ AstroAPI/media_services/music/apple_music/__init__.py
@@ -1,12 +1,13 @@
 from AstroAPI.components import *
 from .components.generic import *
 
-from .components.search.song import search_song
-from .components.search.collection import search_collection
-from .components.search.music_video import search_music_video
+#from .components.search.song import search_song
+#from .components.search.collection import search_collection
+#from .components.search.music_video import search_music_video
 from .components.lookup.song import lookup_song
 from .components.lookup.collection import lookup_collection
-from .components.lookup.music_video import music_video
+#from .components.lookup.music_video import music_video
+from .components.lookup.artist import lookup_artist
 
 class AppleMusic:
 	def __init__(self):
@@ -16,7 +17,7 @@ class AppleMusic:
 	async def search_song(self, artists: list, title: str, song_type: str = None, collection: str = None, is_explicit: bool = None, country_code: str = 'us') -> Song | Empty | Error:
 		song = await search_song(artists = artists, title = title, song_type = song_type, collection = collection, is_explicit = is_explicit, country_code = country_code)
 		return song
-	
+
 	async def search_collection(self, artists: list, title: str, year: int = None, country_code: str = 'us') -> Collection | Empty | Error:
 		collection = await search_collection(artists = artists, title = title, year = year, country_code = country_code)
 		return collection
@@ -24,11 +25,15 @@ class AppleMusic:
 	async def lookup_song(self, id: str, country_code: str = 'us') -> Song | Empty | Error:
 		song = await lookup_song(id = id, country_code = country_code)
 		return song
-	
+
 	async def lookup_collection(self, id: str, country_code: str = 'us') -> Collection | Empty | Error:
 		collection = await lookup_collection(id = id, country_code = country_code)
 		return collection
-	
 
-	
+	async def lookup_artist(self, id: str, country_code: str = 'us') -> Song | Empty | Error:
+		artist = await lookup_artist(id = id, country_code = country_code)
+		return artist
+
+
+
 apple_music = AppleMusic()
