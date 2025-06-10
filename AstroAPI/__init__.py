bb88b780df316f3c0bdc608e51307beffb69429d
r <64286083+worsedillonbrooks@users.noreply.github.com>
2025-06-10 11:27:29 +0200

    separate music and knowledge imports

--- AstroAPI/__init__.py
+++ AstroAPI/__init__.py
@@ -1,4 +1,5 @@
 #from AstroAPI.media_services.global_api import Global, Spotify, AppleMusic, YouTubeMusic, Tidal, Deezer, Genius
-from AstroAPI.media_services.music import *
+from AstroAPI.media_services import music
+from AstroAPI.media_services import knowledge
 from AstroAPI.components.media import Error, Empty, Meta
 print('[AstroAPI] Up with flying colors!')
