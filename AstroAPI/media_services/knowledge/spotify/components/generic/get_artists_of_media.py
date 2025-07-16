4e8da3fa7507cbc5c5cd08eaa26ae9c2a379041c
r <64286083+sushiroles@users.noreply.github.com>
2025-07-16 03:06:42 +0200

    this just cost me an hour of sleep

--- AstroAPI/media_services/knowledge/spotify/components/generic/get_artists_of_media.py
+++ AstroAPI/media_services/knowledge/spotify/components/generic/get_artists_of_media.py
@@ -1,4 +1,1 @@
-from ....genius.components.generic.about import service
-from AstroAPI.components import *
-
 from AstroAPI.media_services.music.spotify.components.generic.get_artists_of_media import get_artists_of_media
