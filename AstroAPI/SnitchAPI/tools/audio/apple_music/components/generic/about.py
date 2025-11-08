c03b8b28aeb7174c5bc5e73a6ef73d6f1dbe4daa
sushi <64286083+sushiroles@users.noreply.github.com>
2025-11-08 23:08:02 +0100

    Specialty Apple Music API for getting preview audio
    The preview audio is being used for checking whether the music is AI generated or not.

---
+++ AstroAPI/SnitchAPI/tools/audio/apple_music/components/generic/about.py
@@ -0,0 +1,3 @@
+service = 'apple_music'
+component = 'Apple Music API (Astro Snitch)'
+api = 'https://itunes.apple.com'
---
+++ AstroAPI/SnitchAPI/tools/audio/apple_music/components/get_song_preview.py
@@ -0,0 +1,80 @@
+from AstroAPI.InternalComponents.Legacy import *
+from AstroAPI.ServiceCatalogAPI.media_services.music.apple_music.components.generic import *
+
+import aiohttp
+
+
+
+async def get_song_preview(id: str, country_code: str = 'us') -> str:
+	# Prepare the request dictionary with song lookup parameters
+	request = {'request': 'get_song_preview', 'id': id, 'country_code': country_code}
+	# Lookup JSON variable for later debugging
+	lookup_json = None
+	# Record the start time for processing time calculation
+	start_time = current_unix_time_ms()
+
+	try:
+		# Create an aiohttp session
+		async with aiohttp.ClientSession() as session:
+			# Prepare for API call
+			api_url = f'{api}/lookup'
+			api_params = {
+				'id': id,
+				'country': country_code.lower()
+			}
+			timeout = aiohttp.ClientTimeout(total = 30) # Set a timeout for the HTTP request
+
+			# Make the GET request to the API endpoint
+			async with session.get(url = api_url, params = api_params, timeout = timeout) as response:
+				lookup_json = await response.json(content_type = 'text/javascript')
+				if response.status == 200:
+					# Parse the JSON response
+					song = lookup_json
+					
+					if song['results'] != []: # Check if there was any song data returned
+						song = song['results'][0]
+						return song['previewUrl']
+
+					else: # If not, return an empty object and log it
+						empty = Empty(
+							service = service,
+							meta = Meta(
+								service = service,
+								request = request,
+								processing_time = current_unix_time_ms() - start_time,
+								http_code = 204
+							)
+						)
+						await log(empty)
+						return empty
+
+				else:
+					error = Error(
+						service = service,
+						component = component,
+						error_msg = "HTTP error when looking up song ID",
+						meta = Meta(
+							service = service,
+							request = request,
+							processing_time = current_unix_time_ms() - start_time,
+							http_code = response.status
+						)
+					)
+					await log(error, [discord.File(fp = StringIO(json.dumps(lookup_json, indent = 4)), filename = f'{id}.json')])
+					return error
+
+	# If sinister things happen
+	except Exception as error:
+		error = Error(
+			service = service,
+			component = component,
+			error_msg = f'Error when looking up collection: "{error}"',
+			meta = Meta(
+				service = service,
+				request = request,
+				http_code = 500,
+				processing_time = {service: current_unix_time_ms() - start_time}
+			)
+		)
+		await log(error, [discord.File(fp = StringIO(json.dumps(lookup_json, indent = 4)), filename = f'{id}.json')])
+		return error
