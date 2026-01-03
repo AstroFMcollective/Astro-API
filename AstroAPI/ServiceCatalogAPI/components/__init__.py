3b0f474a8b0d1e0ee089d036c0483371f2c8bdcd
sushi <64286083+sushiroles@users.noreply.github.com>
2026-01-03 12:17:07 +0100

    Import/refactoring changes

--- AstroAPI/ServiceCatalogAPI/components/__init__.py
+++ AstroAPI/ServiceCatalogAPI/components/__init__.py
@@ -1,1 +1,2 @@
+from AstroAPI.InternalComponents.SystemMediaObjects import *
 from .generic import *
--- AstroAPI/ServiceCatalogAPI/components/generic/__init__.py
+++ AstroAPI/ServiceCatalogAPI/components/generic/__init__.py
@@ -1,3 +1,2 @@
 from AstroAPI.ServiceCatalogAPI.components.generic.media import *
-from AstroAPI.InternalComponents.SystemMediaObjects import *
 from .filtering import *
