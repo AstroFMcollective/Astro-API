a5fe5ecca246a28cd70ea1d42f4b53abc8af49d6
sushi <64286083+sushiroles@users.noreply.github.com>
2026-02-17 00:43:44 +0100

    Edge cases

--- AstroAPI/ServiceCatalogAPI/media_services/music/spotify/components/generic/create_objects/create_collection_objects.py
+++ AstroAPI/ServiceCatalogAPI/media_services/music/spotify/components/generic/create_objects/create_collection_objects.py
@@ -16,48 +16,52 @@ async def create_collection_objects(json_response: dict, request: dict, start_ti
 	collections = []
 
 	# Iterate through each song in the response
-	for collection in json_response['albums']['items']:
-		# Determine the collection type (album or ep)
-		collection_type = ('album' if collection['album_type'] != 'single' else 'ep')
-		collection_url = collection['external_urls']['spotify']
-		collection_id = collection['id']
-		collection_title = remove_feat(collection['name'])
-		collection_artists = get_artists_of_media(request, collection['artists'])
-		collection_year = collection['release_date'][:4]
+	if 'albums' in json_response:
+		for collection in json_response['albums']['items']:
+			# Determine the collection type (album or ep)
+			collection_type = ('album' if collection['album_type'] != 'single' else 'ep')
+			collection_url = collection['external_urls']['spotify']
+			collection_id = collection['id']
+			collection_title = remove_feat(collection['name'])
+			collection_artists = get_artists_of_media(request, collection['artists'])
+			collection_year = collection['release_date'][:4]
 
-		# Create a Cover object for the collection's artwork
-		media_cover = Cover(
-			service = service,
-			media_type = collection_type,
-			title = collection_title,
-			artists = collection_artists,
-			hq_urls = collection['images'][0]['url'] if collection['images'] != [] else None,
-			lq_urls = collection['images'][len(collection['images']) - 1]['url'] if collection['images'] != [] else None,
-			meta = Meta(
+			# Create a Cover object for the collection's artwork
+			media_cover = Cover(
 				service = service,
-				request = request,
-				processing_time = 0,
-				filter_confidence_percentage = 100.0,
-				http_code = 200
-			)
-		)    
+				media_type = collection_type,
+				title = collection_title,
+				artists = collection_artists,
+				hq_urls = collection['images'][0]['url'] if collection['images'] != [] else None,
+				lq_urls = collection['images'][len(collection['images']) - 1]['url'] if collection['images'] != [] else None,
+				meta = Meta(
+					service = service,
+					request = request,
+					processing_time = 0,
+					filter_confidence_percentage = 100.0,
+					http_code = 200
+				)
+			)    
 
-		# Create a Collection object and add it to the list
-		collections.append(Collection(
-			service = service,
-			type = collection_type,
-			urls = collection_url,
-			ids = collection_id,
-			title = collection_title,
-			artists = collection_artists,
-			release_year = collection_year,
-			cover = media_cover,
-			meta = Meta(
+			# Create a Collection object and add it to the list
+			collections.append(Collection(
 				service = service,
-				request = request,
-				processing_time = current_unix_time_ms() - start_time,
-				http_code = http_code
-			)
-		))
+				type = collection_type,
+				urls = collection_url,
+				ids = collection_id,
+				title = collection_title,
+				artists = collection_artists,
+				release_year = collection_year,
+				cover = media_cover,
+				meta = Meta(
+					service = service,
+					request = request,
+					processing_time = current_unix_time_ms() - start_time,
+					http_code = http_code
+				)
+			))
+		
+		return collections
 	
-	return collections
+	else:
+		return [] 
